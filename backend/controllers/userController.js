50bb788fac1d7dca00d853d682a7df31b7f7f5fd
Julia Dlutek <jdlutek987@gmail.com>
2023-10-28 18:45:30 +0200

    Add login endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,25 +1,35 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/userModel');
 
 const registerUser = async (req, res) => {
-  if (req.body.password !== req.body.passwordConfirmation) {
+  const {
+    username,
+    password,
+    passwordConfirmation,
+    hairID,
+    outfitTopID,
+    outfitBottomID,
+  } = req.body;
+
+  if (password !== passwordConfirmation) {
     res.status(400);
     throw new Error('Passwords not match!');
   }
-  const userExists = await User.findOne({ username: req.body.username });
+  const userExists = await User.findOne({ username });
   if (userExists) {
     res.status(400);
-    throw new Error(`User ${req.body.username} already exists`);
+    throw new Error(`User ${username} already exists`);
   }
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  const hashedPassword = await bcrypt.hash(password, salt);
   const user = await User.create({
-    username: req.body.username,
+    username,
     password: hashedPassword,
-    hairID: req.body.hairID,
-    outfitTopID: req.body.outfitTopID,
-    outfitBottomID: req.body.outfitBottomID,
+    hairID,
+    outfitTopID,
+    outfitBottomID,
   });
   if (user) {
     res.status(201).json({
@@ -28,14 +38,35 @@ const registerUser = async (req, res) => {
       hairID: user.hairID,
       outfitTopID: user.outfitTopID,
       outfitBottomID: user.outfitBottomID,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
     throw new Error('Invalid user data');
   }
 };
-const loginUser = (req, res) => {
-  res.send('Login route');
+const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      hairID: user.hairID,
+      outfitTopID: user.outfitTopID,
+      outfitBottomID: user.outfitBottomID,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials');
+  }
+};
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
 };
 
 module.exports = {
