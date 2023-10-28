ff244ae4e6505053e94ad213b138560df12a9883
Julia Dlutek <jdlutek987@gmail.com>
2023-10-28 16:41:10 +0200

    Create database connection

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
-console.log('Server is running...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Test');
+});
+
+app.listen(PORT, () => console.log('Server is running on port ' + PORT));
