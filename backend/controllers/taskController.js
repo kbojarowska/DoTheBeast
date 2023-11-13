dc133fb369d85313f3531f4595b3f31d52d6b919
Julia Dlutek <jdlutek987@gmail.com>
2023-11-13 11:26:50 +0100

    Create controllers draft for todoList and task

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,23 @@
+const Task = require('../models/taskModel');
+
+module.exports = {
+  getAllTasks: async (req, res) => {
+    // ...
+  },
+
+  getTaskById: async (req, res) => {
+    // ...
+  },
+
+  createTask: async (req, res) => {
+    // ...
+  },
+
+  updateTask: async (req, res) => {
+    // ...
+  },
+
+  deleteTask: async (req, res) => {
+    // ...
+  },
+};
---
+++ backend/controllers/todoListController.js
@@ -0,0 +1,23 @@
+const TodoList = require('../models/todoListModel');
+
+module.exports = {
+  getAllTaskLists: async (req, res) => {
+    // ...
+  },
+
+  getTaskListById: async (req, res) => {
+    // ...
+  },
+
+  createTaskList: async (req, res) => {
+    // ...
+  },
+
+  updateTaskList: async (req, res) => {
+    // ...
+  },
+
+  deleteTaskList: async (req, res) => {
+    // ...
+  },
+};
