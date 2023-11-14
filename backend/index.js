d5dc0b701bfdec9f1fb304592afdf533178bc236
Julia Dlutek <jdlutek987@gmail.com>
2023-11-14 10:15:23 +0100

    Comment out problematic lines in index.js

--- backend/index.js
+++ backend/index.js
@@ -13,26 +13,27 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 const options = {
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Do The Beast',
-        version: '1.0.0',
-        description: 'API documentation for Do The Beast Application',
-      },
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Do The Beast',
+      version: '1.0.0',
+      description: 'API documentation for Do The Beast Application',
     },
-    apis: ["./controllers/*.js"],
-  };
+  },
+  apis: ['./controllers/*.js'],
+};
 
 const swaggerSpec = swaggerJsDoc(options);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
-const outputPath = path.resolve('_build', 'swagger-output.json');
-fs.writeFileSync(outputPath, JSON.stringify(swaggerSpec, null, 2));
+// const outputPath = path.resolve('_build', 'swagger-output.json');
+// fs.writeFileSync(outputPath, JSON.stringify(swaggerSpec, null, 2));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', require('./routes/userRoutes'));
+app.use('/todoLists', require('./routes/todoListRoutes'));
 
 app.listen(PORT, () => console.log('Server is running on port ' + PORT));
