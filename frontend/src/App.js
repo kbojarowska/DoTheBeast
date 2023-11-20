6b6836aedf8ae094135a92d7c15b3b6a31dc02a0
Julia Dlutek <jdlutek987@gmail.com>
2023-11-20 12:08:41 +0100

    Create new page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,15 @@ import {
 } from 'react-router-dom'
 import './App.scss'
 import LandingPage from './pages/LandingPage/LandingPage'
+import MainPage from './pages/MainPage/MainPage'
 
 function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path='/' element={<LandingPage />} />
+				{/* TODO: MainPage should also have the path "/". We need to implement AuthProvider to dynamically determine the page to display on the "/" path based on authentication status */}
+				<Route path='/main' element={<MainPage />} />
 			</Routes>
 		</Router>
 	)
