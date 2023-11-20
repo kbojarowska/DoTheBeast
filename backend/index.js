cf484b35b6d29da34fc53d35bc5bb546694bbb9d
kbojarowska <83237280+kbojarowska@users.noreply.github.com>
2023-11-20 18:41:10 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,18 +13,20 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Do The Beast',
-      version: '1.0.0',
-      description: 'API documentation for Do The Beast Application',
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Do The Beast',
+        version: '1.0.0',
+        description: 'API documentation for Do The Beast Application',
+      },
     },
-  },
-  apis: ['./controllers/*.js'],
-};
+    apis: ["./controllers/*.js"],
+  };
 
 const swaggerSpec = swaggerJsDoc(options);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -40,14 +42,9 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', require('./routes/userRoutes'));
-app.use('/todoLists', require('./routes/todoListRoutes'));
-app.use('/tasks', require('./routes/taskRoutes'));
-app.use('/monsters', require('./routes/monsterRoutes'));
 
 app.listen(PORT, () => console.log('Server is running on port ' + PORT));
