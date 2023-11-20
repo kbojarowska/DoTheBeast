6f1d3aad44b96bec975a8a2f271118cf2d5b1b36
kbojarowska <83237280+kbojarowska@users.noreply.github.com>
2023-11-20 14:47:01 +0100

    created monster CRUD, added swagger descriptions for schemas (#40)
    * created monster CRUD, added swagger descriptions for schemas
    
    * upgraded delete monster function
    
    ---------
    
    Co-authored-by: kbojarowska <kbojarowska@wp.pl>

--- backend/index.js
+++ backend/index.js
@@ -46,5 +46,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', require('./routes/userRoutes'));
+app.use('/monsters', require('./routes/monsterRoutes'));
 
 app.listen(PORT, () => console.log('Server is running on port ' + PORT));
