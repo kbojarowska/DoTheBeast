b411d3ec73eca0e1e81bfe4bd1c1483353aff645
zdwo <72862515+zdwo@users.noreply.github.com>
2023-11-21 14:23:11 +0100

    pr fixes and popup added

--- frontend/src/ui/pages/RegisterPage/RegisterPage.js
+++ frontend/src/ui/pages/RegisterPage/RegisterPage.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/jsx-key */
-/* eslint-disable indent */
 import React, { useEffect, useState } from 'react'
 import './RegisterPage.scss'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
@@ -7,120 +5,131 @@ import * as Yup from 'yup'
 import { Button, Text } from '../../components'
 import { register } from '../../../ducks/UserApi'
 import { useNavigate } from 'react-router'
+import RegisterPopup from './RegisterPopup'
 
 function RegisterPage() {
 
-    const navigate = useNavigate()
+	const navigate = useNavigate()
 
-    const [currentBody, setCurrentBody] = useState(0)
-    const [currentHair, setCurrentHair] = useState(0)
-    const [currentFit, setCurrentFit] = useState(0)
+	const [currentBody, setCurrentBody] = useState(0)
+	const [currentHair, setCurrentHair] = useState(0)
+	const [currentFit, setCurrentFit] = useState(0)
+	const [toggle, setToggle] = useState(false)
 
-    const [errors, setErrors] = useState([])
+	const [errors, setErrors] = useState([])
     
 	const registerSchema = Yup.object().shape({
 		password: Yup.string().required('Wait! What\'s your password?'),
-        passwordConfirmation: Yup.string().required('Confirm your passowrd.').oneOf([Yup.ref('password'), null], 'Passwords don\'t match.'),
+		passwordConfirmation: Yup.string().required('Confirm your passowrd.').oneOf([Yup.ref('password'), null], 'Passwords don\'t match.'),
 		username: Yup.string().required('Wait! What\'s your username?')
 	})
 
 	const handleSubmit = async (values) => {
-        const avatar = {
-            'body': /^([^.]+)/.exec(Object.keys(body)[currentBody])[1],
-            'hair': /^([^.]+)/.exec(Object.keys(hair)[currentHair])[1],
-            'fit': /^([^.]+)/.exec(Object.keys(fit)[currentFit])[1]
-        }
+		const avatar = {
+			'body': /^([^.]+)/.exec(Object.keys(body)[currentBody])[1],
+			'hair': /^([^.]+)/.exec(Object.keys(hair)[currentHair])[1],
+			'fit': /^([^.]+)/.exec(Object.keys(fit)[currentFit])[1]
+		}
 
-        const user = {...values, hairID: parseInt(avatar.hair),  outfitTopID: parseInt(avatar.fit), outfitBottomID: parseInt(avatar.body)}
+		const user = {...values, hairID: parseInt(avatar.hair),  outfitTopID: parseInt(avatar.fit), outfitBottomID: parseInt(avatar.body)}
 
-        const res = await register(user)
-        if (!res) {
-            setErrors(['Server not available'])
-        } else {
-            if (res.status === 200) {
-                navigate('/login')
-            } else if (res.status === 401) {
-                setErrors([res.data.message, ...errors])
-            }
-        }
+		const res = await register(user)
+		if (!res) {
+			setErrors(['Server not available'])
+		} else {
+			if (res.status === 201) {
+				setToggle(true)
+				setTimeout(() => {
+					navigate('/login')
+				}, 3000)
+			} else if (res.status === 400) {
+				setTimeout(() => {
+					setErrors([])
+				}, 4000)
+				setErrors([res.data.message, ...errors])
+			}
+		}
 	}
 
-    function importAll(r) {
-        let images = {}
-        r.keys().forEach((item) => { images[item.replace('./', '')] = r(item) })
-        return images
-    }
+	function importAll(r) {
+		let images = {}
+		r.keys().forEach((item) => { images[item.replace('./', '')] = r(item) })
+		return images
+	}
     
-    // eslint-disable-next-line no-undef
-    const body = importAll(require.context('../../assets/avatar_files/body', false, /\.(png|jpe?g|svg)$/))
-    // eslint-disable-next-line no-undef
-    const fit = importAll(require.context('../../assets/avatar_files/fit', false, /\.(png|jpe?g|svg)$/))
-    // eslint-disable-next-line no-undef
-    const hair = importAll(require.context('../../assets/avatar_files/hair', false, /\.(png|jpe?g|svg)$/))
+	// eslint-disable-next-line no-undef
+	const body = importAll(require.context('../../assets/avatar_files/body', false, /\.(png|jpe?g|svg)$/))
+	// eslint-disable-next-line no-undef
+	const fit = importAll(require.context('../../assets/avatar_files/fit', false, /\.(png|jpe?g|svg)$/))
+	// eslint-disable-next-line no-undef
+	const hair = importAll(require.context('../../assets/avatar_files/hair', false, /\.(png|jpe?g|svg)$/))
 
-    useEffect(() => {
+	useEffect(() => {
 
-    }, [currentBody, currentFit, currentHair])
+	}, [currentBody, currentFit, currentHair])
 
 
 	return (
-		<div className='register-page'>
-            <div className='register-container'>
-                <Formik
-                    initialValues={{ username: '', password: '' , passwordConfirmation: ''}}
-                    validationSchema={registerSchema}
-                    onSubmit={(values, { resetForm }) => {return (handleSubmit(values), resetForm())}}>
-                    {() => {
-                        return (
-                            <Form className='register-form'>
-                                <p className='header'>SIGNUP</p>
-                                <Text className='register-field-box'>
-                                    <Field className="register-field" placeholder="USERNAME" type="username" name="username" />
-                                    <ErrorMessage className='error-msg' name="username" component="div" />
-                                </Text>
-                                <Text className='register-field-box'>
-                                    <Field className="register-field" placeholder="PASSWORD" type="password" name="password" />
-                                    <ErrorMessage className='error-msg' name="password" component="div" />
-                                </Text>
-                                <Text className='register-field-box'>
-                                    <Field className="register-field" placeholder="CONFIRM YOUR PASSWORD" type="password" name="passwordConfirmation" />
-                                    <ErrorMessage className='error-msg' name="passwordConfirmation" component="div" />
-                                    {errors ? errors.map(e => <div className='error-msg'>{e}</div>) : null}
-                                </Text>
-                                <Button className='register-btn'><button className='register-btn btn' type="submit">CREATE MY ACCOUNT</button></Button>
-                            </Form>
-                        )
-                    }}
-                </Formik>
-            </div>
-            <div className="avatar-register">
-                <div className="create-avatar">
-                    <div className="window">
-                        <img className='body' alt="body" src={body[Object.keys(body)[currentBody]]} />
-                        <img className='hair' alt="hair" src={hair[Object.keys(hair)[currentHair]]} />
-                        <img className='fit' alt="fit" src={fit[Object.keys(fit)[currentFit]]} />
-                    </div>
-                </div>
-                <div className="create-avatar">
-                    <div className="buttons">
-                        <div className="select">
-                            <button onClick={() => { setCurrentBody((Object.keys(body).length + (currentBody - 1)) % Object.keys(body).length) }}><Text>&#8249;</Text></button>
-                            <Text>BODY</Text>
-                            <button onClick={() => { setCurrentBody((currentBody + 1) % Object.keys(body).length) }}><Text>&#8250;</Text></button>
-                        </div>
-                        <div className="select">
-                            <button onClick={() => { setCurrentHair((Object.keys(hair).length + (currentHair - 1)) % Object.keys(hair).length) }}><Text>&#8249;</Text></button>
-                            <Text>HAIR</Text>
-                            <button onClick={() => { setCurrentHair((currentHair + 1) % Object.keys(hair).length) }}><Text>&#8250;</Text></button>
-                        </div>
-                        <div className="select">
-                            <button onClick={() => { setCurrentFit((Object.keys(fit).length + (currentFit - 1)) % Object.keys(fit).length) }}><Text>&#8249;</Text></button>
-                            <Text>FIT</Text>
-                            <button onClick={() => { setCurrentFit((currentFit + 1) % Object.keys(fit).length) }}><Text>&#8250;</Text></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+		<div className={`register-page ${toggle ? 'overlay' : null}`}> 
+			{(toggle) ? 
+				<RegisterPopup /> : null
+			}
+			<div className='register-container'>
+				<Formik
+					initialValues={{ username: '', password: '' , passwordConfirmation: ''}}
+					validationSchema={registerSchema}
+					onSubmit={(values, { resetForm }) => {return (handleSubmit(values), resetForm())}}>
+					{() => {
+						return (
+							<Form className='register-form'>
+								<p className='header'>SIGNUP</p>
+								<Text className='register-field-box'>
+									<Field className="register-field" placeholder="USERNAME" type="username" name="username" />
+									<ErrorMessage className='error-msg' name="username" component="div" />
+								</Text>
+								<Text className='register-field-box'>
+									<Field className="register-field" placeholder="PASSWORD" type="password" name="password" />
+									<ErrorMessage className='error-msg' name="password" component="div" />
+								</Text>
+								<Text className='register-field-box'>
+									<Field className="register-field" placeholder="CONFIRM YOUR PASSWORD" type="password" name="passwordConfirmation" />
+									<ErrorMessage className='error-msg' name="passwordConfirmation" component="div" />
+									{errors ? errors.map(e => <div key={e} className='error-msg'>{e}</div>) : null}
+								</Text>
+								<Button className='register-btn'><button className='register-btn btn' type="submit">CREATE MY ACCOUNT</button></Button>
+							</Form>
+						)
+					}}
+				</Formik>
+			</div>
+			<div className="avatar-register">
+				<div className="create-avatar">
+					<div className="window">
+						<img className='body' alt="body" src={body[Object.keys(body)[currentBody]]} />
+						<img className='hair' alt="hair" src={hair[Object.keys(hair)[currentHair]]} />
+						<img className='fit' alt="fit" src={fit[Object.keys(fit)[currentFit]]} />
+					</div>
+				</div>
+				<div className="create-avatar">
+					<div className="buttons">
+						<div className="select">
+							<button onClick={() => { setCurrentBody((Object.keys(body).length + (currentBody - 1)) % Object.keys(body).length) }}><Text>&#8249;</Text></button>
+							<Text>BODY</Text>
+							<button onClick={() => { setCurrentBody((currentBody + 1) % Object.keys(body).length) }}><Text>&#8250;</Text></button>
+						</div>
+						<div className="select">
+							<button onClick={() => { setCurrentHair((Object.keys(hair).length + (currentHair - 1)) % Object.keys(hair).length) }}><Text>&#8249;</Text></button>
+							<Text>HAIR</Text>
+							<button onClick={() => { setCurrentHair((currentHair + 1) % Object.keys(hair).length) }}><Text>&#8250;</Text></button>
+						</div>
+						<div className="select">
+							<button onClick={() => { setCurrentFit((Object.keys(fit).length + (currentFit - 1)) % Object.keys(fit).length) }}><Text>&#8249;</Text></button>
+							<Text>FIT</Text>
+							<button onClick={() => { setCurrentFit((currentFit + 1) % Object.keys(fit).length) }}><Text>&#8250;</Text></button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
