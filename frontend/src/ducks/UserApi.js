344ede3ea5763ee38d8a372d881fef11b02da05c
zdwo <72862515+zdwo@users.noreply.github.com>
2023-11-21 11:04:10 +0100

    changed server config in UserApi

--- frontend/src/ducks/UserApi.js
+++ frontend/src/ducks/UserApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
 	baseURL: 'https://do-the-beast-1r3q.onrender.com',
-	withCredentials: true,
+	withCredentials: false,
 	headers: {
 		Accept: 'application/json',
 		'Content-Type': 'application/json'
