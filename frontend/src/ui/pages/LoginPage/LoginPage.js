75de76a054f459d77e1a99ba9ef77e32a905dbcd
zdwo <72862515+zdwo@users.noreply.github.com>
2023-11-21 14:23:00 +0100

    pr fixes

--- frontend/src/ui/pages/LoginPage/LoginPage.js
+++ frontend/src/ui/pages/LoginPage/LoginPage.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/jsx-key */
-/* eslint-disable indent */
 import React, { useState } from 'react'
 import './LoginPage.scss'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
@@ -10,54 +8,54 @@ import { useNavigate } from 'react-router'
 
 function LoginPage() {
 
-    const navigate = useNavigate()
+	const navigate = useNavigate()
 
 	const loginSchema = Yup.object().shape({
 		password: Yup.string().required('Wait! What\'s your password?'),
 		username: Yup.string().required('Wait! What\'s your username?')
 	})
 
-    const [errors, setErrors] = useState([])
+	const [errors, setErrors] = useState([])
 
 	const handleSubmit = async (values) => {
-        const res = await login(values)
-        if (!res) {
-            setErrors(['Server not available'])
-        } else {
-            if (res.status === 200) {
-                navigate('/') // zmienić na user_page
-            } else if (res.status === 401) {
-                setErrors([res.data.message, ...errors])
-            }
-        }
+		const res = await login(values)
+		if (!res) {
+			setErrors(['Server not available'])
+		} else {
+			if (res.status === 200) {
+				navigate('/') // zmienić na user_page
+			} else if (res.status === 401) {
+				setErrors([res.data.message, ...errors])
+			}
+		}
 	}
 
 	return (
 		<div>
-            <div className='login-container'>
-                <Formik
-                    initialValues={{ username: '', password: '' }}
-                    validationSchema={loginSchema}
-                    onSubmit={(values, { resetForm }) => {return (handleSubmit(values), resetForm())}}>
-                    {() => {
-                        return (
-                            <Form className='login-form'>
-                                <p className='header'>LOGIN</p>
-                                <Text className='login-field-box'>
-                                    <Field id='username' className="login-field" placeholder="USERNAME" type="username" name="username" />
-                                    <ErrorMessage className='error-msg' name="username" component="div" />
-                                </Text>
-                                <Text className='login-field-box'>
-                                    <Field id='password' className="login-field" placeholder="PASSWORD" type="password" name="password" />
-                                    <ErrorMessage className='error-msg' name="password" component="div" />
-                                    {errors ? errors.map(e => <div className='error-msg'>{e}</div>) : null}
-                                </Text>
-                                <Button className='login-btn'><button className='login-btn btn' type="submit">LET ME IN!</button></Button>
-                            </Form>
-                        )
-                    }}
-                </Formik>
-            </div>
+			<div className='login-container'>
+				<Formik
+					initialValues={{ username: '', password: '' }}
+					validationSchema={loginSchema}
+					onSubmit={(values, { resetForm }) => {return (handleSubmit(values), resetForm())}}>
+					{() => {
+						return (
+							<Form className='login-form'>
+								<p className='header'>LOGIN</p>
+								<Text className='login-field-box'>
+									<Field id='username' className="login-field" placeholder="USERNAME" type="username" name="username" />
+									<ErrorMessage className='error-msg' name="username" component="div" />
+								</Text>
+								<Text className='login-field-box'>
+									<Field id='password' className="login-field" placeholder="PASSWORD" type="password" name="password" />
+									<ErrorMessage className='error-msg' name="password" component="div" />
+									{errors ? errors.map(e => <div key={e} className='error-msg'>{e}</div>) : null}
+								</Text>
+								<Button className='login-btn'><button className='login-btn btn' type="submit">LET ME IN!</button></Button>
+							</Form>
+						)
+					}}
+				</Formik>
+			</div>
 		</div>
 	)
 }
