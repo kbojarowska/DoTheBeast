c23fed08ac2c0c634d1f463955bb19ac9289a185
zdwo <72862515+zdwo@users.noreply.github.com>
2023-12-22 13:38:29 +0100

    button

--- frontend/src/ui/pages/LoginPage/LoginPage.js
+++ frontend/src/ui/pages/LoginPage/LoginPage.js
@@ -53,7 +53,7 @@ function LoginPage() {
 									<ErrorMessage className='error-msg' name="password" component="div" />
 									{errors && errors.map(e => <div key={e} className='error-msg'>{e}</div>)}
 								</Text>
-								<Button className='login-btn'>LET ME IN!</Button>
+								<Button type='submit' className='login-btn'>LET ME IN!</Button>
 								<Link className='link-login-reg' to='/register'>Don&apos;t have an account? Sign up.</Link>
 							</Form>
 						)
