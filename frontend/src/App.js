de7cbe6fdd569eeae2ca2970c51621d707b13335
Bojarowska <katarzyna.bojarowska@intel.com>
2024-01-11 14:16:39 +0100

    created burger menu component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,19 @@ import LandingPage from './ui/pages/LandingPage/LandingPage'
 import MainPage from './ui/pages/MainPage/MainPage'
 import UserPage from './ui/pages/UserPage/UserPage'
 import FriendsPage from './ui/pages/FriendsPage/FriendsPage'
+import { BurgerMenu } from './ui/components'
 
 function App() {
+	const links = [
+		{ href: '/', text: 'Home' },
+		{ href: '/main', text: 'Main' },
+		{ href: '/userpage', text: 'User Page' },
+		{ href: '/friends', text: 'Friends' }
+	]
+
 	return (
 		<Router>
+			<BurgerMenu links={links} />
 			<Routes>
 				<Route path='/' element={<LandingPage />} />
 				{/* TODO: MainPage should also have the path "/". We need to implement AuthProvider to dynamically determine the page to display on the "/" path based on authentication status */}
