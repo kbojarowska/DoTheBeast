310917f79b7cd8a09eaeead08d146cc0d6a42c1b
kbojarowska <kbojarowska@wp.pl>
2024-01-15 13:45:22 +0100

    added cookie for session

--- frontend/src/ducks/AuthProvider.js
+++ frontend/src/ducks/AuthProvider.js
@@ -1,26 +1,44 @@
-import React, { useState, createContext, useContext } from 'react'
+import React, { useEffect, useState, createContext, useContext } from 'react'
 import PropTypes from 'prop-types'
 import * as userApi from './UserApi'
+import Cookies from 'js-cookie'
 
 const AuthContext = createContext(null)
 
 export const AuthProvider = ({ children }) => {
 	const [user, setUser] = useState(null)
 
+	useEffect(() => {
+		const userCookie = Cookies.get('user')
+		if (userCookie) {
+			const userFromCookie = JSON.parse(userCookie)
+			setUser(userFromCookie)
+		}
+	}, [])
+
 	const login = async (values) => {
 		try {
 			const response = await userApi.login(values)
-			setUser(response.data)
+			const userData = response.data
+	
+			if (userData) {
+				setUser(userData)
+				Cookies.set('user', JSON.stringify(userData), { expires: 7 })
+			} else {
+				console.error('Login error: User data not found in the response')
+			}
+	
 			return response
 		} catch (error) {
 			console.error('Login error:', error)
-			return error.response
+			return error.response || { error: 'Unknown error occurred during login' }
 		}
 	}
 
 	const logout = async () => {
 		try {
 			setUser(null)
+			Cookies.remove('user')
 		} catch (error) {
 			console.error('Logout error:', error)
 		}
