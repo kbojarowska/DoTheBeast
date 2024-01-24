b894c8b401fb0f03a79f9a35011b9bab63bf3edd
Julia Dłutek <75124863+juliadlutek@users.noreply.github.com>
2024-01-24 21:32:27 +0100

    62 fe create add a friend page (#64)
    * create search field
    
    * create endpoint for searching users
    
    * wyszukiwanie na froncie, buttony dodawania i usuwania
    
    * create buttons to handle friend add / remove
    
    * change db base url
    
    * Correct tooltip visibility
    
    * Change api path

--- frontend/src/ui/pages/FriendsPage/FriendsPage.js
+++ frontend/src/ui/pages/FriendsPage/FriendsPage.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Loading, Text } from '../../components'
+import { Button, Loading, Text } from '../../components'
 import { Tooltip } from 'react-tooltip'
-import { getUserById } from '../../../ducks/UserApi'
+import { getUserById, getUsersByUsername, addFriend, removeFriend } from '../../../ducks/UserApi'
 import PropTypes from 'prop-types'
 import './FriendsPage.scss'
 
@@ -11,19 +11,30 @@ function FriendsPage() {
 	const [userData, setUserData] = useState(null)
 	const [friendsData, setFriendsData] = useState(null)
 	const [currentIndex, setCurrentIndex] = useState(0)
+	const [searchQuery, setSearchQuery] = useState('')
 
 	const showFriends = friendsData?.slice(currentIndex, currentIndex + 4) 
 
 	useEffect(() => {
-		getUserById(userId)
-			.then((data) => {
-				setUserData(data)
-				setFriendsData(data.friends)
-			})
-			.catch((error) => {
-				console.error(error)
-			})
-	}, [userId])
+		if (searchQuery.trim() !== '') {
+			getUsersByUsername(searchQuery)
+				.then((data) => {
+					setFriendsData(data.map(user => user._id).filter(id => id !== userData._id))
+				})
+				.catch((error) => {
+					console.error(error)
+				})
+		} else {
+			getUserById(userId)
+				.then((data) => {
+					setUserData(data)
+					setFriendsData(data.friends)
+				})
+				.catch((error) => {
+					console.error(error)
+				})
+		}
+	}, [userId, searchQuery])
 
 	function importAll(r) {
 		let images = {}
@@ -129,7 +140,18 @@ function FriendsPage() {
 				</div>
 				<div className="friends-list">
 					{showFriends.map((friendId, index) => (
-						<FriendAvatar key={index} friendId={friendId} />
+						<div key={index}>
+							<FriendAvatar friendId={friendId} />
+							{(userData.friends.includes(friendId)) ?
+								<Button className="delete-btn" onClick={() => {
+									removeFriend({'userId': userData._id, 'friendId': friendId})
+									setUserData({...userData, friends: userData.friends.filter(id => id !== friendId)})
+								}} >Usuń znajomego</Button> : 
+								<Button className="add-btn" onClick={() => {
+									addFriend({'userId': userData._id, 'friendId': friendId})
+									setUserData({...userData, friends: [...userData.friends, friendId]})
+								}}>Dodaj znajomego</Button>}
+						</div>
 					))}
 				</div>
 				<div className="arrow-buttons">
@@ -154,6 +176,11 @@ function FriendsPage() {
 					<div className="hello-user">
 						<Text className="hello-text">{userData.username} friends</Text>
 					</div>	
+					<input type="text"
+						className='text-field'
+						placeholder='Search for users'
+						value={searchQuery}
+						onChange={(e) => setSearchQuery(e.target.value)} />
 					<div className="friends-container">
 						<div className="avatar-register">
 							<div className="create-avatar">
